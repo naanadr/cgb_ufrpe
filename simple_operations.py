c112a7f72a0fbc941c1a7f342b66b3fce783dc8a
FernandaSzAdr <fsandrade25@gmail.com>
2020-03-14 19:38:25 -0300

    Adiciona código que executa os testes na biblioteca

---
+++ simple_operations.py
@@ -0,0 +1,14 @@
+from exercicios_teste import (letter_a, letter_b, letter_c, letter_d,
+                              letter_e, letter_f, letter_g, letter_h)
+
+
+if __name__ == '__main__':
+    letter_a()
+    print('\n')
+    letter_b()
+    print('\n')
+    letter_c()
+    print('\n')
+    letter_d()
+    print('\n')
+    letter_e()
