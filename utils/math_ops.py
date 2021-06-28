4ee77480109a8fe389d3852143a86087b4b686f5
naanadr <fsandrade25@gmail.com>
2021-06-27 23:02:40 -0300

    Adiciona novas funções ao math_ops.py

--- utils/math_ops.py
+++ utils/math_ops.py
@@ -47,7 +47,7 @@ def base_ortonormal(V, N):
     return [norm_U, norm_V, norm_N]
 
 
-def _coord_vista(P, config, base_ortonormal):
+def coord_vista(P, config, base_ortonormal):
     """
     Essa função converte um ponto em coordenada mundial para coordenada de vista:
 
@@ -57,10 +57,10 @@ def _coord_vista(P, config, base_ortonormal):
     return np.dot(base_ortonormal, np.subtract(P, config.get("C")))
 
 
-def _coordenadas_tela(coord_vista, config, res_x, res_y):
+def coordenadas_tela(coordenadas_vista, config, res_x, res_y):
     coordenadas_normalizadas = [
-        config.get("d/hx") * (coord_vista[0] / coord_vista[2]),
-        config.get("d/hy") * (coord_vista[1] / coord_vista[2]),
+        config.get("d/hx") * (coordenadas_vista[0] / coordenadas_vista[2]),
+        config.get("d/hy") * (coordenadas_vista[1] / coordenadas_vista[2]),
     ]
 
     return [
@@ -69,33 +69,84 @@ def _coordenadas_tela(coord_vista, config, res_x, res_y):
     ]
 
 
-def convert_coord(P, config, base_ortonormal, res_x, res_y):
-    """
-    A partir de uma coordenada mundial, é realizada a conversão para coordenada
-    de vista e depois para coordenada de tela.
-    """
-    coord_vista = _coord_vista(P, config, base_ortonormal)
-    coord_tela = _coordenadas_tela(coord_vista, config, res_x, res_y)
-
-    return coord_tela
-
-
-def find_normal(value1, value2, value3, config, base_ortonormal):
+def find_normal_triangle(coordenadas_vista):
     """
     Calcula a normal de uma superfice a partir de três valores e normaliza esse
     resultado.
 
-    1 - Se for um triangulo, utilizar as três vertices;
-    1.1 - Calcular o produto vetorial de v2 - v1 e v3 - v1
-    1.2 - Utilizar a regra da mão esquerda, para fazer o produto vetorial
-    1.3 - Normalizar o vetor resultante
+    Para um determinado triangulo, utilizar as três vertices;
+    1 - Calcular o produto vetorial de v2 - v1 e v3 - v1
+    2 - Utilizar a regra da mão esquerda, para fazer o produto vetorial
+    3 - Normalizar o vetor resultante
     """
-    value1 = _coord_vista(value1, config, base_ortonormal)
-    value2 = _coord_vista(value2, config, base_ortonormal)
-    value3 = _coord_vista(value3, config, base_ortonormal)
+    value1, value2, value3 = coordenadas_vista
 
     v1 = np.subtract(value2, value1)
     v2 = np.subtract(value3, value1)
     prod_vetorial = np.cross(v1, v2)
 
     return _normalizar(prod_vetorial)
+
+
+def find_normal_vertice(vertice):
+    """
+    Para um determinado vertice, é encontrado a normal dele de acordo com as
+    normais dos triangulos que utilizam esse vertice.
+    """
+    norm_triangles = [t.normal for t in vertice.triangles]
+    return _normalizar(np.sum(norm_triangles, axis=0))
+
+
+def find_baricentro_triangulo(vertices, config, base_ortonormal):
+    vertice_x, vertice_y, vertice_z = vertices
+
+    return [sum(group) / 3 for group in zip(vertice_x, vertice_y, vertice_z)]
+
+
+def _find_baricentro_ponto(ponto, vertice_x, vertice_y, vertice_z):
+    """
+    A partir de um ponto P (em coordenada de tela) obtido pelo scan line, e
+    dos vertices que compõe a superfice onde está esse ponto P, é realizada a
+    conversão do R2 para o R3 desse ponto utilizando as coordenadas baricentricas.
+    """
+    a = vertice_x[0] - vertice_z[0]
+    b = vertice_y[0] - vertice_z[0]
+    c = vertice_x[1] - vertice_z[1]
+    d = vertice_y[1] - vertice_z[1]
+
+    try:
+        T = np.array(
+            [
+                [a, b],
+                [c, d],
+            ]
+        )
+        T_inv = np.linalg.inv(T)
+        matrix = np.array([[ponto[0] - vertice_z[0]], [ponto[1] - vertice_z[1]]])
+        alfa, beta = np.matmul(T_inv, matrix)
+        gama = 1 - alfa - beta
+
+        return (alfa[0], beta[0], gama[0])
+    except Exception as e:
+        import ipdb
+
+        ipdb.set_trace()
+        return (0, 0, 0)
+
+
+def find_p_original(ponto, coordenadas_tela, coordenadas_vista):
+    alfa, beta, gama = _find_baricentro_ponto(
+        ponto,
+        coordenadas_tela[0],
+        coordenadas_tela[1],
+        coordenadas_tela[2],
+    )
+
+    return np.sum(
+        [
+            np.dot(alfa, coordenadas_vista[0]),
+            np.dot(beta, coordenadas_vista[1]),
+            np.dot(gama, coordenadas_vista[2]),
+        ],
+        axis=0,
+    )
