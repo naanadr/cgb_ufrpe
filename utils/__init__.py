adc87a4e535da353d770abe05fc578489b9f528b
naanadr <fsandrade25@gmail.com>
2021-06-27 23:03:03 -0300

    Adiciona novas funções ao utils/__init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
+import copy
 from os import getenv
 
 import cv2
@@ -6,7 +7,15 @@ from dotenv import load_dotenv
 from objects.malha3d import Malha3D
 from objects.triangle import Triangle
 from objects.vertex import Vertex
-from utils.math_ops import convert_coord, find_normal
+from utils.bresenhan import find_points
+from utils.math_ops import (
+    coord_vista,
+    coordenadas_tela,
+    find_baricentro_triangulo,
+    find_normal_triangle,
+    find_normal_vertice,
+)
+from utils.scanline import fill_poly
 
 
 def read_file(file_name):
@@ -77,18 +86,82 @@ def enrich_triangles(malha3d, **kwargs):
     no atributo `normal`
     """
     for triangle in malha3d.triangles:
-        point_x = malha3d.vertices[triangle.index_x].get()
-        point_y = malha3d.vertices[triangle.index_y].get()
-        point_z = malha3d.vertices[triangle.index_z].get()
+        vertice_x = malha3d.vertices[triangle.index_x]
+        vertice_x.triangles.append(triangle)
+        vertice_y = malha3d.vertices[triangle.index_y]
+        vertice_y.triangles.append(triangle)
+        vertice_z = malha3d.vertices[triangle.index_z]
+        vertice_z.triangles.append(triangle)
 
-        triangle.normal = find_normal(
-            point_x, point_y, point_z, kwargs["config"], kwargs["base_ortonormal"]
-        )
-        triangle.vector = [
-            convert_coord(P=point_x, **kwargs),
-            convert_coord(P=point_y, **kwargs),
-            convert_coord(P=point_z, **kwargs),
+        triangle.vertices_coord_vista = [
+            coord_vista(
+                vertice_x.get(),
+                kwargs["config"],
+                kwargs["base_ortonormal"],
+            ),
+            coord_vista(
+                vertice_y.get(),
+                kwargs["config"],
+                kwargs["base_ortonormal"],
+            ),
+            coord_vista(
+                vertice_z.get(),
+                kwargs["config"],
+                kwargs["base_ortonormal"],
+            ),
         ]
+        triangle.vertices_coord_tela = [
+            coordenadas_tela(
+                triangle.vertices_coord_vista[0],
+                kwargs["config"],
+                kwargs["res_x"],
+                kwargs["res_y"],
+            ),
+            coordenadas_tela(
+                triangle.vertices_coord_vista[1],
+                kwargs["config"],
+                kwargs["res_x"],
+                kwargs["res_y"],
+            ),
+            coordenadas_tela(
+                triangle.vertices_coord_vista[2],
+                kwargs["config"],
+                kwargs["res_x"],
+                kwargs["res_y"],
+            ),
+        ]
+        triangle.normal = find_normal_triangle(triangle.vertices_coord_vista)
+        triangle.baricentro = find_baricentro_triangulo(
+            triangle.vertices_coord_vista, kwargs["config"], kwargs["base_ortonormal"]
+        )
+
+
+def enrich_vertices(malha3d):
+    for vertice in malha3d.vertices:
+        vertice.normal = find_normal_vertice(vertice)
+
+
+def find_pixels(triangle):
+    vertices = copy.deepcopy(triangle.vertices_coord_tela)
+    vertices.sort(key=lambda x: x[1])
+    # Lista com pares de coordenadas
+    # Cada par representa o começo e o fim de um lateral do triangulo
+    # No total são 3 pares de coordenadas
+    coords_sides = []
+    for i in range(0, len(triangle.vertices_coord_tela) - 1):
+        coords_sides.append((vertices[i], vertices[i + 1]))
+    coords_sides.append((vertices[0], vertices[-1]))
+
+    # Encontra os pontos que foram as retas (lados) do triangulo
+    coords_inside_sides = []
+    for point in coords_sides:
+        coords_inside_sides.append(find_points(p0=point[0], p1=point[1]))
+
+    return fill_poly(
+        points=triangle.vertices_coord_tela,
+        sides=coords_sides,
+        coords=coords_inside_sides,
+    )
 
 
 def draw(img, point):
