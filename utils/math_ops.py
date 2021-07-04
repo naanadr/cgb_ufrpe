1c27d9c0091a861791bbd2a4b6d1e64eeadc9142
naanadr <fsandrade25@gmail.com>
2021-07-04 12:50:44 -0300

    Atualiza as funções no math_ops.py
    Atualiza: find_p_original(), find_baricentro_ponto(), find_baricentro_triangulo(), coordenadas_tela(), base_ortonormal()

--- utils/math_ops.py
+++ utils/math_ops.py
@@ -1,4 +1,4 @@
-from math import sqrt
+from math import floor, sqrt
 
 import numpy as np
 
@@ -38,13 +38,11 @@ def base_ortonormal(V, N):
     4 - Retorna a base ortonormal [U, V, N]
     """
     V = _ortogonalizar(V, N)
+    V = _normalizar(V)
+    N = _normalizar(N)
     U = _build_u_value(N, V)
 
-    norm_V = _normalizar(V)
-    norm_N = _normalizar(N)
-    norm_U = _normalizar(U)
-
-    return [norm_U, norm_V, norm_N]
+    return [U, V, N]
 
 
 def coord_vista(P, config, base_ortonormal):
@@ -59,13 +57,16 @@ def coord_vista(P, config, base_ortonormal):
 
 def coordenadas_tela(coordenadas_vista, config, res_x, res_y):
     coordenadas_normalizadas = [
-        config.get("d/hx") * (coordenadas_vista[0] / coordenadas_vista[2]),
-        config.get("d/hy") * (coordenadas_vista[1] / coordenadas_vista[2]),
+        (config.get("d") * coordenadas_vista[0]) / (res_x * coordenadas_vista[2]),
+        (config.get("d") * coordenadas_vista[1]) / (res_y * coordenadas_vista[2]),
     ]
 
+    const_i = ((coordenadas_normalizadas[0] + 1) / 2) * res_x
+    const_j = ((coordenadas_normalizadas[1] + 1) / 2) * res_y
+
     return [
-        int(((coordenadas_normalizadas[0] + 1) / 2) * (res_x + 0.5)),
-        int(res_y - ((coordenadas_normalizadas[1] + 1) / 2) * res_y + 0.5),
+        floor(const_i + 0.5),
+        floor(res_y - const_j + 0.5),
     ]
 
 
@@ -98,55 +99,43 @@ def find_normal_vertice(vertice):
 
 
 def find_baricentro_triangulo(vertices, config, base_ortonormal):
-    vertice_x, vertice_y, vertice_z = vertices
+    vertice_a, vertice_b, vertice_c = vertices
+    return [sum(group) / 3 for group in zip(vertice_a, vertice_b, vertice_c)]
 
-    return [sum(group) / 3 for group in zip(vertice_x, vertice_y, vertice_z)]
 
-
-def _find_baricentro_ponto(ponto, vertice_x, vertice_y, vertice_z):
+def find_baricentro_ponto(ponto, vertice_a, vertice_b, vertice_c):
     """
     A partir de um ponto P (em coordenada de tela) obtido pelo scan line, e
     dos vertices que compõe a superfice onde está esse ponto P, é realizada a
     conversão do R2 para o R3 desse ponto utilizando as coordenadas baricentricas.
     """
-    a = vertice_x[0] - vertice_z[0]
-    b = vertice_y[0] - vertice_z[0]
-    c = vertice_x[1] - vertice_z[1]
-    d = vertice_y[1] - vertice_z[1]
+    a = vertice_a[0] - vertice_c[0]
+    b = vertice_b[0] - vertice_c[0]
+    c = vertice_a[1] - vertice_c[1]
+    d = vertice_b[1] - vertice_c[1]
 
-    try:
-        T = np.array(
-            [
-                [a, b],
-                [c, d],
-            ]
-        )
-        T_inv = np.linalg.inv(T)
-        matrix = np.array([[ponto[0] - vertice_z[0]], [ponto[1] - vertice_z[1]]])
-        alfa, beta = np.matmul(T_inv, matrix)
-        gama = 1 - alfa - beta
+    matrix = np.array([[ponto[0] - vertice_c[0]], [ponto[1] - vertice_c[1]]])
+    T = [
+        [a, b],
+        [c, d],
+    ]
+    det = np.linalg.det(T)
+    if det == 0.0:
+        return False
 
-        return (alfa[0], beta[0], gama[0])
-    except Exception as e:
-        import ipdb
+    T_inv = np.linalg.inv(T)
+    alfa, beta = np.matmul(T_inv, matrix)
+    gama = 1 - alfa - beta
 
-        ipdb.set_trace()
-        return (0, 0, 0)
+    return (alfa[0], beta[0], gama[0])
 
 
-def find_p_original(ponto, coordenadas_tela, coordenadas_vista):
-    alfa, beta, gama = _find_baricentro_ponto(
-        ponto,
-        coordenadas_tela[0],
-        coordenadas_tela[1],
-        coordenadas_tela[2],
-    )
+def find_p_original(coordenada_barizentrica, coordenadas_vista):
+    alfa, beta, gama = coordenada_barizentrica
+    vertice_a, vertice_b, vertice_c = coordenadas_vista
 
-    return np.sum(
-        [
-            np.dot(alfa, coordenadas_vista[0]),
-            np.dot(beta, coordenadas_vista[1]),
-            np.dot(gama, coordenadas_vista[2]),
-        ],
-        axis=0,
-    )
+    return [
+        np.sum([alfa * vertice_a[0], beta * vertice_b[0], gama * vertice_c[0]]),
+        np.sum([alfa * vertice_a[1], beta * vertice_b[1], gama * vertice_c[1]]),
+        np.sum([alfa * vertice_a[2], beta * vertice_b[2], gama * vertice_c[2]]),
+    ]
