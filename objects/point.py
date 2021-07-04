eab1fd00c2cc65937fdf43be2d89930bd598421a
naanadr <fsandrade25@gmail.com>
2021-07-04 12:51:10 -0300

    Adiciona classe Point

---
+++ objects/point.py
@@ -0,0 +1,5 @@
+class Point:
+    def __init__(self, pixel, coord_baricentrica, coord_original):
+        self.pixel = pixel
+        self.coord_baricentrica = coord_baricentrica
+        self.p_original = coord_original
